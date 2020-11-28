b1388c401432d7b329693265eb488432a9645d83
Ashish Katiyar <74372244+ashishkatiyar2404@users.noreply.github.com>
2020-11-29 01:07:30 +0530

    Done-Only some CSS left

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputdiv = document.querySelector("#output");
- 
+var outputDiv = document.querySelector("#output");
 
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server try again after some time")
+}
 
 function clickHandler() {
-   outputdiv.innerText="hahahahahaha" +txtInput.value;
+//    outputdiv.innerText="hahahahahaha" +txtInput.value;
+        var inputText  = txtInput.value;  // taking input  
+
+        //calling server for processing
+        fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText  = json.contents.translated;
+            outputDiv.innerText  = translatedText;  //output
+        })
+        .catch(errorHandler)
 };
 btnTranslate.addEventListener("click", clickHandler)
 
