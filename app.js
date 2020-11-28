20857e8599dd4b58dd247284173840dd732b624e
Ashish Katiyar <74372244+ashishkatiyar2404@users.noreply.github.com>
2020-11-28 16:28:23 +0530

    I/p and o/p processing done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput)
+var outputdiv = document.querySelector("#output");
+ 
+
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log("input",txtInput.value);
+   outputdiv.innerText="hahahahahaha" +txtInput.value;
 };
 btnTranslate.addEventListener("click", clickHandler)
 
